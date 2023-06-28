06a21094066632c3929ed6792c68eaf8480b4101
Esdras Silva <esdraslimasilva83@gmail.com>
2023-06-28 08:49:49 -0300

    Make home page responsible (mobile)

--- app/components/MenuTags.tsx
+++ app/components/MenuTags.tsx
@@ -2,11 +2,35 @@
 
 import styles from "@/styles/home.module.scss";
 import TagItem from "./TagItem";
+import { useEffect, useRef, useState } from "react";
+
+import { GiHamburgerMenu } from "react-icons/gi";
+import { GrClose } from "react-icons/gr";
 
 //Contains all tags allowed to be choosen
 export default function MenuTags() {
+    const menuRef = useRef<HTMLDivElement | null>(null);
+
+    const toggleMenuTags = () => {
+        const menu = menuRef.current as HTMLDivElement;
+        if (menu) {
+            menu.classList.toggle(styles.hiddenMenu);
+        }
+    };
+
+    useEffect(() => {
+        setTimeout(() => {}, 3000);
+    }, []);
+
     return (
-        <div className={styles.menuCateogories}>
+        <div
+            ref={menuRef}
+            className={`${styles.menuCateogories} ${styles.hiddenMenu}`}
+        >
+            <button type="button" onClick={toggleMenuTags}>
+                <GiHamburgerMenu />
+            </button>
+
             <ul>
                 <TagItem tag="all" />
                 <TagItem tag="ui" />
